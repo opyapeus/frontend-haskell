fa043a4a1ba04017e4b89d94c4d7792a6f78cabf
opyapeus <opyapeus@gmail.com>
2018-11-09 12:51:58 +0900

    refactor

--- src/IdClass.hs
+++ src/IdClass.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE FunctionalDependencies           #-}
 {-# LANGUAGE FlexibleInstances           #-}
 
 module IdClass where
@@ -16,27 +15,27 @@ import           Language.Sunroof               ( JSString
                                                 )
 
 -- | Type class for attribute types of each DSLs
-class Show a => ToAttribute a b | b -> a where
-    toAttr :: a -> b
-
--- | Id and Class type
-data IdClass = Id I | Class C
-    deriving (Show, Eq)
+class ToAttribute a where
+    toAttr :: IdClass -> a
 
 -- | Define toAttr function of html DSL
-instance ToAttribute IdClass Attribute where
+instance ToAttribute Attribute where
     toAttr (Id v) = id_ . pack . kebab . show $ v
     toAttr (Class v) = class_ . pack . kebab. show $ v
 
 -- | Define toAttr function of css DSL
-instance ToAttribute IdClass Selector where
+instance ToAttribute Selector where
     toAttr (Id v) = selectorFromText . mappend "#" . pack . kebab . show $ v
     toAttr (Class v) = selectorFromText . mappend "." . pack . kebab . show $ v
 
 -- | Define toAttr function of js DSL
-instance ToAttribute IdClass JSString where
+instance ToAttribute JSString where
     toAttr (Id v) = string . mappend "#" . kebab . show $ v
     toAttr (Class v) = string . mappend "." . kebab . show $ v
+
+-- | Id and Class type
+data IdClass = Id I | Class C
+    deriving (Show, Eq)
 
 -- | Define your ids
 data I
@@ -48,4 +47,3 @@ data C
     = Container
     | Button
     deriving (Show, Eq)
-
